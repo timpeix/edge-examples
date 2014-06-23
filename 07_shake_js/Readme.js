4d2193e6b610151c0f811dac6123798f3b87cd4f
Sebastian Schürmann <sebs@crom.localdomain>
2014-06-24 00:38:18 +0200

    Docs

--- 07_shake_js/Readme.js
+++ 07_shake_js/Readme.js
@@ -3,5 +3,11 @@
 Adobe Edge example for 
 
 * Using shake.js to control animations shaking the ipad
-* animation with greensock.js
+* how to capture a shake event
+* load a 3rd part library
+
+
+## Links
+
+* https://github.com/alexgibson/shake.js/
 
